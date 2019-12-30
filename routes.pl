c2a3cd10e870618ec9c4b2cc313bb93bf7ffd070
Drew K Goodin <dkg@cksum.io>
2019-12-29 18:16:50 -0800

    default error route

--- routes.pl
+++ routes.pl
@@ -1,12 +1,22 @@
-noroute sub {
+noroute( sub {
   status(404);
-  type('text');
+  type('plain');
   template('error')
-};
+});
 
 route '/', get => sub {
   status(200);
   type('html'); 
   template('index')
 };
+
+route '/api', get => sub {
+  my %hsh = (hi => 'world');
+  status(201);
+  type('json');
+  require JSON;
+  my $json = JSON->new();
+  $json->encode(\%hsh)
+};
+
 
